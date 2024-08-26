e06aafa1a93a313a8b3fa70f0c87e8fd28bc2a92
Ahmshere <asus555post@gmail.com>
2024-08-26 08:16:12 +0200

    pbt.main2

--- src/main/java/config/BaseTest.java
+++ src/main/java/config/BaseTest.java
@@ -23,30 +23,33 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters("browser")
-    public  void setUp(@Optional("chrome") String browser){
+    public void setUp(@Optional("chrome") String browser) {
         System.out.println("Setting up browser: " + browser);
-        if (browser.equalsIgnoreCase("chrome")){
+        if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            ChromeOptions option  = new ChromeOptions();
+            ChromeOptions option = new ChromeOptions();
             option.addArguments("--lang=en");
             driver = new ChromeDriver(option);
-        }
-        else if(browser.equalsIgnoreCase("firefox")){
+        } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             FirefoxOptions option = new FirefoxOptions();
-            option.addPreference("intl.accept_languages","en");
+            option.addPreference("intl.accept_languages", "en");
             driver = new FirefoxDriver(option);
-        }
-        else if(browser.equalsIgnoreCase("safari")){
+        } else if (browser.equalsIgnoreCase("safari")) {
             driver = new SafariDriver();
+        } else {
+            throw new IllegalArgumentException("Invalid browser value: " + browser);
         }
-        else {
-            throw new IllegalArgumentException("Invalid browser value : "+browser);}
-       // driver = getDriver();
+
+        if (driver == null) {
+            System.out.println("Driver is not initialized!");
+        } else {
+            System.out.println("Driver initialized successfully.");
+        }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //BasePage.setDriver(driver);
     }
 
 
